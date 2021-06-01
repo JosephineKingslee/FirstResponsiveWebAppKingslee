b1c834cb8388c6a4b7976b06cce95ed3a11db291
JosephineKingslee <85074778+JosephineKingslee@users.noreply.github.com>
2021-05-31 22:26:57 -0500

    FirstResponsiveWebAppKingslee

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppKingslee.Models;
+
+namespace FirstResponsiveWebAppKingslee.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FRE = 0;
+            ViewBag.FR = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FirstResponsiveWebApp model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FRE = model.AgeThisYearEnd();
+                ViewBag.FR = model.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.FRE = 0;
+                ViewBag.FR = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/FirstResponsiveWebApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppKingslee.Models
+{
+    public class FirstResponsiveWebApp
+    {
+        [Required(ErrorMessage = "Please enter User's name.")]
+        //[RegularExpression("[a-zA-Z]+")]
+        public string Name { get; set; }
+        public DateTime BirthYear { get; set; }
+        public DateTime BirthYearEnd = Convert.ToDateTime("12/31/2021");
+        public DateTime todayDate = DateTime.Today;
+        public int AgeThisYear()
+        {
+            int age = todayDate.Year - BirthYear.Year;
+
+            if (BirthYear > todayDate.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+        public int AgeThisYearEnd()
+        {
+            int ageEnd = BirthYearEnd.Year - BirthYear.Year;
+            if (BirthYear > BirthYearEnd.AddYears(-ageEnd))
+            {
+                ageEnd--;
+            }
+            return ageEnd;
+        }
+    }
+}
